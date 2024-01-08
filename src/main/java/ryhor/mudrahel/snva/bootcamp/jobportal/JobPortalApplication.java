8669c7b3f8e167da41b88882d9da76d59ab22e0e
Ryhor Mudrahel <ryhor.murahel@gmail.com>
2024-01-08 11:14:04 -0500

    Initial commit

---
+++ src/main/java/ryhor/mudrahel/snva/bootcamp/jobportal/JobPortalApplication.java
@@ -0,0 +1,13 @@
+package ryhor.mudrahel.snva.bootcamp.jobportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ryhor/mudrahel/snva/bootcamp/jobportal/JobPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package ryhor.mudrahel.snva.bootcamp.jobportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
