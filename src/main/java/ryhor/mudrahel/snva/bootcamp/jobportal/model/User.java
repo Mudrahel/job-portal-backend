ac7a26879a3a2473d43e14295d6fdc5d3df84c4f
Ryhor Mudrahel <ryhor.murahel@gmail.com>
2024-01-08 11:19:48 -0500

    Added User entity and DB table as result

---
+++ src/main/java/ryhor/mudrahel/snva/bootcamp/jobportal/model/User.java
@@ -0,0 +1,51 @@
+package ryhor.mudrahel.snva.bootcamp.jobportal.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "username")
+    private  String username;
+
+    @Column(name = "password")
+    private  String password;
+
+    public User() {
+    }
+
+    public User(long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
